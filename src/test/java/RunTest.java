61dad42c6b218af7904125e14cb607eca6b6c114
irina.dureikina <89271264888@ya.ru>
2023-09-14 12:01:03 +0300

    add mainOpenTest

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,7 +1,13 @@
 import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.title;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import iFellow.*;
+
+import java.util.Objects;
 
 public class RunTest {
 
@@ -39,6 +45,16 @@ public class RunTest {
         System.out.println("Kelvin test");
         double result = new BaseConverter(100, 2).convert();
         assertEquals(373.15, result);
+    }
+
+    @Test
+    public void mainOpenTest() {
+        System.out.println(" Main open Demoqa test");
+
+        open("https://demoqa.com/automation-practice-form");
+
+        String pageURL = getWebDriver().getCurrentUrl();
+        Assertions.assertTrue(pageURL.contains("https://demoqa.com/automation-practice-form"), "Не верный URL");
     }
 
 }
