94c2f5005d427fdb850e6676973b258d6c2bbb9c
IrinaDureykina <79271264888@ya.ru>
2023-09-20 18:27:29 +0400

    Update Open_Url.java

--- src/test/java/Page_Objects/Page_Elements/Edujira_start_page.java
+++ src/test/java/Page_Objects/Page_Elements/Edujira_start_page.java
@@ -6,7 +6,6 @@ public class Edujira_start_page {
 
     public static SelenideElement home_page_button_href = $x("//span[@id='logo']/a[contains(@aria-label,'Перейти на главную страницу')]");
 
-
     public static SelenideElement input_Login = $x("//input[@id='login-form-username' and @name='os_username']");
 
     public static SelenideElement input_Password = $x("//input[@id='login-form-password' and @name='os_password']");
