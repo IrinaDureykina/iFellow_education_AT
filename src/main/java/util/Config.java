538550ca85daf915ca48ed016015f7ec6305ea61
irina.dureikina <89271264888@ya.ru>
2023-09-26 22:23:47 +0400

    Update Config.java

--- src/main/java/util/Config.java
+++ src/main/java/util/Config.java
@@ -1,10 +1,11 @@
 package util;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class Config {
@@ -13,7 +14,8 @@ public class Config {
     private static final Properties prop = new Properties();
 
     static {
-        try (InputStream input = new FileInputStream(CONFIG_FILE_PATH)) {
+
+        try (InputStream input = Files.newInputStream(Paths.get(CONFIG_FILE_PATH))) {
             prop.load(new InputStreamReader(input, StandardCharsets.UTF_8));
         } catch (IOException ex) {
             System.err.println("Ошибка при загрузке конфигурации: " + ex.getMessage());
